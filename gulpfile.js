20da66a4c4ed61dceafaf9280b369b0f3697ac2e
Meathill <meathill@gmail.com>
2017-07-02 21:58:49 +0800

    调整好技能模块

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,26 @@
 
 const gulp = require('gulp');
 const imageMin = require('gulp-imagemin');
+const stylus = require('gulp-stylus');
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
 
 gulp.task('image', () => {
   gulp.src('src/img/**')
     .pipe(imageMin())
     .pipe(gulp.dest('img/'));
+});
+
+gulp.task('stylus', () => {
+  gulp.src('styl/screen.styl')
+    .pipe(stylus({
+      'include css': true
+    }))
+    .pipe(cleanCSS({
+      level: 2
+    }))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('css/'))
 });
