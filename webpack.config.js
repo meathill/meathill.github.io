6e95825525eb0470ff106f4ad50828471f14ec02
Meathill <meathill@gmail.com>
2020-03-14 10:26:13 +0800

    change: upgraded deps, removed useless deps

--- webpack.config.js
+++ webpack.config.js
@@ -23,10 +23,6 @@ module.exports = {
         loader: 'pug-loader',
       },
       {
-        test: /\.hbs$/,
-        use: 'handlebars-loader',
-      },
-      {
         test: /\.styl(us)?$/,
         use: [
           'style-loader',
