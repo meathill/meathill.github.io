5e4980b81a89a3b45c2dedde3d92c90e8edb6296
Meathill <meathill@gmail.com>
2020-04-06 10:33:21 +0800

    deps: upgraded deps

--- webpack.config.js
+++ webpack.config.js
@@ -38,6 +38,7 @@ module.exports = {
             options: {
               name: '[name].[ext]',
               outputPath: 'assets',
+              esModule: false,
             },
           },
         ],
