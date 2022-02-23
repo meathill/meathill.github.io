2fe6c5344fc204d928d94b150c463c9f870c0c87
Meathill <meathill@gmail.com>
2022-02-23 09:34:08 +0800

    change: updated GA code

--- src/index.js
+++ src/index.js
@@ -29,11 +29,8 @@ new Blog({
 });
 
 // GA
-(function(i,s,o,g,r,a){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o);
-  a.async=1;a.src=g;
-  document.body.append(a);
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+const dataLayer = window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
 
-ga('create', 'UA-26694288-7', 'auto');
-ga('send', 'pageview');
+gtag('config', 'UA-26694288-7');
